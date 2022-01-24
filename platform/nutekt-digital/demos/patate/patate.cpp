b99146b14681916bbfa83ddf063f0bb910d6f461
roybossofpesto <andreduracell@gmail.com>
2022-01-24 04:03:15 +0100

    good to go

--- platform/nutekt-digital/demos/patate/patate.cpp
+++ platform/nutekt-digital/demos/patate/patate.cpp
@@ -6,7 +6,8 @@
 struct State {
   bool is_on = false;
 
-  const float* wave0 = wavesC[0];
+  float vol0 = 1.f;
+  size_t index0 = 0;
   float phi0 = 0.f;
 };
 
@@ -22,6 +23,11 @@ void OSC_CYCLE(
   int32_t* yy_,
   const uint32_t frames)
 {
+  const float* wave0 =
+    state.index0 % 4 == 0 ? wavesA[0] :
+    state.index0 % 4 == 1 ? wavesB[0] :
+    state.index0 % 4 == 2 ? wavesC[0] :
+    wavesD[0];
 
   const auto w0 = osc_w0f_for_note(
     (params->pitch)>>8,
@@ -30,9 +36,8 @@ void OSC_CYCLE(
   auto yy = static_cast<q31_t*>(yy_);
   auto yy_end = yy + frames;
   for (; yy < yy_end; yy++) {
-    const float sig = !state.is_on ? osc_white() : osc_wave_scanf(state.wave0, state.phi0);
-    // sig += ;
-
+    float sig = !state.is_on ? osc_white() : osc_wave_scanf(wave0, state.phi0);
+    sig *= state.vol0;
 
     *yy = f32_to_q31(sig);
 
@@ -46,17 +51,30 @@ void OSC_NOTEON(
   const user_osc_param_t* const params)
 {
   state.is_on = true;
-  // s_waves.state.flags |= Waves::k_flag_reset;
 }
 
 void OSC_NOTEOFF(
   const user_osc_param_t* const params)
 {
   state.is_on = false;
-  // (void)params;
 }
 
 void OSC_PARAM(uint16_t index, uint16_t value)
 {
+  static const uint8_t k_a_thr = k_waves_a_cnt;
 
+  switch (index) {
+  case k_user_osc_param_id1:
+    state.index0 = value % 4;
+    break;
+  case k_user_osc_param_id2:
+    // state.index0 = value % 4;
+    break;
+  case k_user_osc_param_shape:
+    state.vol0 = clip01f(value * 0.01f);
+    break;
+  case k_user_osc_param_shiftshape:
+    // state.vol0 = clip01f(value * 0.01f);
+    break;
+  }
 }
