089a6b1b879987097a8d9e94147bbecd0768f9b0
roybossofpesto <andreduracell@gmail.com>
2022-01-24 01:57:33 +0100

    bueni

--- platform/nutekt-digital/demos/patate/patate.cpp
+++ platform/nutekt-digital/demos/patate/patate.cpp
@@ -1,10 +1,10 @@
 #include "userosc.h"
 
-uint8_t patate(uint8_t xx)
-{
-  return 42;
-}
+struct State {
+  bool is_on = false;
+};
 
+State state;
 
 void OSC_INIT(uint32_t /*platform*/, uint32_t /*api*/)
 {
@@ -14,23 +14,31 @@ void OSC_INIT(uint32_t /*platform*/, uint32_t /*api*/)
 
 void OSC_CYCLE(
   const user_osc_param_t* const params,
-  int32_t *yn,
+  int32_t* yy_,
   const uint32_t frames)
 {
 
+  auto yy = static_cast<q31_t*>(yy_);
+  auto yy_end = yy + frames;
+
+  for (size_t kk=0; kk<frames; kk++) {
+    const float sig = state.is_on ? osc_white() : 0.f;
+    yy[kk] = f32_to_q31(sig);
+  }
 
 }
-
 
 void OSC_NOTEON(
   const user_osc_param_t* const params)
 {
+  state.is_on = true;
   // s_waves.state.flags |= Waves::k_flag_reset;
 }
 
 void OSC_NOTEOFF(
   const user_osc_param_t* const params)
 {
+  state.is_on = false;
   // (void)params;
 }
 
