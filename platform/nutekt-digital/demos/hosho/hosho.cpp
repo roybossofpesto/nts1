e4f387ea873dc4edd2c0327b7b9a2b371aa66f69
roybossofpesto <andreduracell@gmail.com>
2022-02-01 06:21:58 +0100

    beat_index

--- platform/nutekt-digital/demos/hosho/hosho.cpp
+++ platform/nutekt-digital/demos/hosho/hosho.cpp
@@ -24,11 +24,12 @@ constexpr float master_gain = 10.f;
 
 struct State {
   float time = 0.f;
-  uint32_t index = 0;
+  uint32_t beat_index = 0;
   float noise_mix = .5f;
+  uint32_t count = 0;
 };
 
-State state;
+static State state;
 
 void OSC_INIT(uint32_t /*platform*/, uint32_t /*api*/)
 {
@@ -56,23 +57,26 @@ void OSC_CYCLE(
 
   auto yy = static_cast<q31_t*>(yy_);
   auto yy_end = yy + frames;
+  float sig = 0.f;
   for (; yy < yy_end; yy++) {
-    float sig = 0.f;
 
-    const Item& item = hosho_items[state.index % std::tuple_size<Items>::value];
-    const bool is_on = state.time < (item.gate_hold.count() + lfo);
-    const float vol =
-      is_on ? item.vol * attack_shape(state.time, item.gate_attack.count()) :
-      0.f;
+    const auto get_signal = [&lfo](const State& state_) -> float {
+      const Item& item = hosho_items[state_.beat_index % std::tuple_size<Items>::value];
+      const bool is_on = state_.time < (item.gate_hold.count() + lfo);
+      const float vol =
+        is_on ? item.vol * attack_shape(state_.time, item.gate_attack.count()) :
+        0.f;
+      const float current = osc_white() * state_.noise_mix + osc_shaped_noise() * (1.f - state_.noise_mix);
+      return  vol * current;
+    };
 
-
-    const float current = osc_white() * state.noise_mix + osc_shaped_noise() * (1.f - state.noise_mix);
-    sig += vol * current;
+    sig = state.count % 10 == 0 ? get_signal(state) : sig;
     sig *= master_gain;
 
     *yy = f32_to_q31(sig);
 
     state.time += k_samplerate_recipf;
+    state.count ++;
   }
 }
 
@@ -80,8 +84,8 @@ void OSC_NOTEON(
   const user_osc_param_t* const params)
 {
   state.time = 0;
-  state.index ++;
-  state.index %= 3;
+  state.beat_index ++;
+  state.beat_index %= 3;
 }
 
 void OSC_NOTEOFF(
@@ -90,9 +94,9 @@ void OSC_NOTEOFF(
   // state.vol0 = 0.f;
 }
 
-void OSC_PARAM(uint16_t index, uint16_t value)
+void OSC_PARAM(uint16_t beat_index, uint16_t value)
 {
-  switch (index) {
+  switch (beat_index) {
   case k_user_osc_param_id1:
     state.noise_mix = value / 99.f;
     break;
