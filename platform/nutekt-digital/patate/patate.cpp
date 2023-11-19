7490151e6596ffb1007b60fd8b1d8a7bd696901d
Pierre Gueth <gueth@adobe.com>
2023-11-19 19:38:34 +0100

    sounds nice enough

--- platform/nutekt-digital/patate/patate.cpp
+++ platform/nutekt-digital/patate/patate.cpp
@@ -22,21 +22,18 @@
 
 struct State
 {
-  static constexpr const size_t max_channels = 16;
+  static constexpr const size_t max_channels = 3;
 
-  float volume = 0.f;
   float disto_amount = 1.f;
   size_t wave_index = 0;
 
   struct Channel
   {
-    uint16_t pitch = 0;
+    float volume = 1.f;
     float dphi = 0.f;
     float phi = 0.f;
   };
 
-  // size_t num_channels = 0;
-  // size_t kk = 0;
   std::array<Channel, max_channels> channels;
 };
 
@@ -45,7 +42,6 @@ State state;
 void OSC_INIT(uint32_t /*platform*/, uint32_t /*api*/)
 {
   state = State();
-  // state.num_channels = 1;
 }
 
 const float *get_wavetable(size_t index)
@@ -74,31 +70,14 @@ void OSC_CYCLE(
     const uint32_t frames)
 {
   const float *wave = get_wavetable(state.wave_index);
-  const uint16_t pitch = params->pitch;
 
-  // bool found_any = false;
-  // for (const auto channel : state.channels)
-  //   if (pitch == channel.pitch)
-  //     found_any = true;
+  const auto dphi = osc_w0f_for_note(
+      params->pitch >> 8,
+      params->pitch & 0xFF);
 
-  // if (!found_any)
-  {
-    const auto dphi = osc_w0f_for_note(
-        pitch >> 8,
-        pitch & 0xFF);
-    // state.num_channels %= State::max_channels;
-    // state.channels[0] = State::Channel{
-    //     pitch,
-    //     dphi,
-    //     0,
-    // };
-
-    for (auto &channel : state.channels) {
-      state.channels[0].dphi = dphi;
-    }
-
-    // state.num_channels++;
-  }
+  state.channels[0].dphi = 1.0 * dphi;
+  state.channels[1].dphi = .5f * dphi;
+  state.channels[2].dphi = .666f * dphi;
 
   auto yy = static_cast<q31_t *>(yy_);
   auto yy_end = yy + frames;
@@ -107,48 +86,30 @@ void OSC_CYCLE(
 
     // accumulate channel signals
     float sig = 0.f;
-    size_t kk = 0;
-    for (const auto &channel : state.channels) {
-      // if (kk++ >= state.num_channels)
-      //   continue;
-      sig += state.volume * osc_wave_scanf(wave, channel.phi);
-    }
+    for (const auto &channel : state.channels)
+      sig += channel.volume * osc_wave_scanf(wave, channel.phi);
     sig /= State::max_channels;
 
     // apply disto
-    sig = osc_softclipf(0.0f, state.disto_amount * sig);
+    state.disto_amount = std::max(state.disto_amount, 1.f);
     sig = osc_softclipf(0.1f, state.disto_amount * sig);
-    // sig = osc_sat_schetzenf(state.disto_amount * sig);
 
     *yy = f32_to_q31(sig);
 
-    // kk = 0;
-    for (auto &channel : state.channels) {
-      // if (kk++ >= state.num_channels)
-      //   continue;
+    for (auto &channel : state.channels)
       channel.phi += channel.dphi;
-    }
   }
 }
 
 void OSC_NOTEON(
     const user_osc_param_t *const params)
 {
-  // auto &channel = state.channels[state.channel_index];
-  // channel.vol = 1.f;
-  // channel.phi = 0.f;
-  state.volume = 1.f;
+  state.channels[0].phi = 0.f;
 }
 
 void OSC_NOTEOFF(const user_osc_param_t *const params)
 {
-  // auto &channel = state.channels[state.channel_index];
-  state.volume = 0.f;
-  // state.num_channels = 0;
-
-  // channel.vol = 0.f;
-  // state.channel_index++;
-  // state.channel_index %= State::num_channels;
+  // state.channels[0].volume = 0.f;
 }
 
 void OSC_PARAM(uint16_t index, uint16_t value)
@@ -159,11 +120,14 @@ void OSC_PARAM(uint16_t index, uint16_t value)
     state.wave_index = value % 4;
     break;
   case k_user_osc_param_shape:
-    // state.volume = clip01f(value * 0.01f);
-    // state.wave_index = value % 4;
-    break;
+  {
+    float aa = param_val_to_f32(value);
+    state.channels[1].volume = (aa < .5) ? (2 * aa) : 1;
+    state.channels[2].volume = (aa < .5) ? 0 : (2 * aa - 1);
+  }
+  break;
   case k_user_osc_param_shiftshape:
-    state.disto_amount = pow(10.f, 12.f * param_val_to_f32(value));
+    state.disto_amount = pow(10.f, 4 * param_val_to_f32(value));
     break;
   }
 }
