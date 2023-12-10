29649fe4b1329a18604388da0c47159f558a8149
Pierre Gueth <gueth@adobe.com>
2023-12-10 21:54:57 +0100

    good enough to test

--- platform/nutekt-digital/patate/custom/src/interp.cpp
+++ platform/nutekt-digital/patate/custom/src/interp.cpp
@@ -1,11 +1,14 @@
 #include <custom/interp.h>
 
+#include "float_math.h"
+
 float custom::linearstep(const float left, const float right, const float xx)
 {
-    return 0;
+    return clip01f((xx - left) / (right - left));
 }
 
 float custom::smoothstep(const float left, const float right, const float xx)
 {
-    return 0;
+    const auto aa = clip01f((xx - left) / (right - left));
+    return 3 * aa * aa - 2 * aa * aa * aa;
 }
--- platform/nutekt-digital/patate/patate.cpp
+++ platform/nutekt-digital/patate/patate.cpp
@@ -1,5 +1,6 @@
 #include "userosc.h"
-#include <custom/mersenne.h>
+// #include <custom/mersenne.h>
+#include <custom/interp.h>
 #include <array>
 
 struct State
@@ -22,18 +23,18 @@ struct State
 
   std::array<Channel, max_channels> channels;
 
-  struct Drone
-  {
-    float frequency = 0.f;
-    float position = 0.f;
-    size_t top_next_rng = 0;
-  };
+  // struct Drone
+  // {
+  //   float frequency = 0.f;
+  //   float position = 0.f;
+  //   size_t top_next_rng = 0;
+  // };
 
-  Drone drone;
+  // Drone drone;
 };
 
 static State state = State();
-static custom::MersenneTwister rng = custom::MersenneTwister();
+// static custom::MersenneTwister rng = custom::MersenneTwister();
 
 void OSC_INIT(uint32_t /*platform*/, uint32_t /*api*/)
 {
@@ -74,40 +75,50 @@ void OSC_CYCLE(
 
   state.channels[0].frequency = 1.f / 1.f * frequency_main;
   state.channels[1].frequency = 1.f / 2.f * frequency_main;
-  state.channels[2].frequency = 2.f / 3.f * frequency_main;
-  state.channels[3].frequency = 4.f / 5.f * frequency_main;
+  state.channels[2].frequency = 3.f / 2.f * frequency_main;
+  state.channels[3].frequency = 5.f / 4.f * frequency_main;
 
-  if (state.drone.top_next_rng < state.top_now)
-  {
-    const auto note_rng = rng.uniform_uchar(120, 150);
-    state.drone.frequency = osc_w0f_for_note(note_rng, 0);
-    state.drone.top_next_rng = state.top_now + k_samplerate * 1.0f;
-  }
+  // if (state.drone.top_next_rng < state.top_now)
+  // {
+  //   const auto note_rng = rng.uniform_uchar(120, 150);
+  //   state.drone.frequency = osc_w0f_for_note(note_rng, 0);
+  //   state.drone.top_next_rng = state.top_now + k_samplerate * 1.0f;
+  // }
 
   auto yy = static_cast<q31_t *>(yy_);
   auto yy_end = yy + frames;
   for (; yy < yy_end; yy++)
   {
-    const float delta = static_cast<float>(state.top_now - state.top_last_noteon) * k_samplerate_recipf;
-    const float release = exp(-delta / (state.shape_amount * .075f + .015f));
+    // const float delta = static_cast<float>(state.top_now - state.top_last_noteon) * k_samplerate_recipf;
+    // const float release = exp(-delta / (state.shape_amount * .075f + .015f));
+    // state.channels[0].volume = release;
+    // state.channels[1].volume = release;
+    // state.channels[2].volume = .5f * release;
+    // state.channels[3].volume = .25f * release;
 
-    state.channels[0].volume = release;
-    state.channels[1].volume = release;
-    state.channels[2].volume = .5f * release;
-    state.channels[3].volume = .25f * release;
+    const float foo = clip01f(state.shape_amount);
+    const float bar = clip01f(state.shiftshape_amount);
+    state.channels[0].volume = 1;
+    state.channels[1].volume = custom::smoothstep(.1f, .3f, foo);
+    state.channels[2].volume = custom::smoothstep(.4f, .6f, foo);
+    state.channels[3].volume = custom::smoothstep(.7f, .9f, foo);
 
     // accumulate channel signals
     float sig = 0.f;
     float vol = 0.f;
     for (const auto &channel : state.channels)
     {
-      sig += channel.volume * osc_cosf(channel.position);
+      const float aa = osc_sinf(channel.position);
+      const float bb = osc_bl_sawf(channel.position, state.wave_index);
+      const float cc = linintf(clip01f(state.shiftshape_amount), aa, bb);
+      sig += channel.volume * cc;
       vol += channel.volume;
     }
-    sig /= state.channels.size();
+    if (vol)
+      sig /= vol;
 
     // sig += channel.volume * osc_wave_scanf(wave, channel.position);
-    sig += state.shiftshape_amount * osc_bl_sawf(state.drone.position, state.wave_index);
+    // sig += state.shiftshape_amount * ;
 
     // // apply disto
     // state.disto_amount = std::max(state.disto_amount, 1.f);
@@ -118,7 +129,7 @@ void OSC_CYCLE(
     for (auto &channel : state.channels)
       channel.position += channel.frequency;
 
-    state.drone.position += state.drone.frequency;
+    // state.drone.position += state.drone.frequency;
 
     state.top_now += 1;
   }
@@ -151,7 +162,6 @@ void OSC_PARAM(uint16_t index, uint16_t value)
     break;
   case k_user_osc_param_shiftshape:
     state.shiftshape_amount = param_val_to_f32(value);
-    // state.wave_index = value % 7;
     break;
   }
 }
