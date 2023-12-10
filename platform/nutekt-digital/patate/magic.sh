6998807cf61ae71735dc20349d646045d2dfbba9
Pierre Gueth <gueth@adobe.com>
2023-12-10 16:42:01 +0100

    dsf

--- platform/nutekt-digital/patate/magic.sh
+++ platform/nutekt-digital/patate/magic.sh
@@ -13,21 +13,25 @@ else
   midi_dump="$( awk '/NTS-1/{ print $1":"$2; }' <<< "${midi_dump}" )"
   midi_inport="$( echo "${midi_dump}" | awk 'BEGIN { FS=":"; } /in:([0-9]+):/{ print $2; }' )"
   midi_outport="$( echo "${midi_dump}" | awk 'BEGIN { FS=":"; } /out:([0-9]+):/{ print $2; }' )"
-  amidi_ioport="$( amidi -l | awk '/NTS-1/ {print $2}' )"
   echo "midi inport '${midi_inport}' outport '${midi_outport}'"
-  # echo "amidi '${amidi_ioport}'"
   if ! [[ ${midi_inport} =~ ^[0-9]+$ ]]; then echo "invalid midi in"; fi
   if ! [[ ${midi_outport} =~ ^[0-9]+$ ]]; then echo "invalid midi outport"; fi
-  # if ! [[ ${amidi_ioport} =~ hw:[0-9,]+$ ]]; then echo "invalid amidi ioport"; fi
+
   logue-cli probe --inport ${midi_inport} --outport ${midi_outport} -v
-  
-  sleep 1
+
   echo "use native tool"
   logue-cli load --unit patate.ntkdigunit --slot 9 --inport ${midi_inport} --outport ${midi_outport}
 
-  echo "use amidi"
-  # logue_dump="$( logue-cli load --unit patate.ntkdigunit --slot 12 --inport ${midi_inport} --outport ${midi_outport} --debug 2> /dev/null || true )"
-  # echo "${logue_dump}" | awk 'END {gsub(/[[:punct:]]/, ""); print}' > foo.sysex
+  # amidi_ioport="$( amidi -l | awk '/NTS-1/ {print $2}' )"
+  # echo "amidi '${amidi_ioport}'"
+  # if ! [[ ${amidi_ioport} =~ hw:[0-9,]+$ ]]; then echo "invalid amidi ioport"; fi
+  
+  # echo "use amidi"
+  # logue_dump="$( logue-cli load -m osc --unit patate.ntkdigunit --slot 9 --inport ${midi_inport} --outport ${midi_outport} --debug 2> /dev/null || true )"
+  # amidi_sysexhex="$( echo "${logue_dump}" | awk '/size/{got_size=1;}; got_size&&/>>> {.*}/{print;}' | awk '{gsub(/[[:punct:]]/, ""); print;}' )"
+  # #  | awk 'END {gsub(/[[:punct:]]/, ""); print}' > foo.sysex
+  # echo "**${amidi_sysexhex}**"
+  # amidi -p "${amidi_ioport}" -S "${amidi_sysexhex}"
 
   logue-cli probe --inport ${midi_inport} --outport ${midi_outport} -v
 fi
